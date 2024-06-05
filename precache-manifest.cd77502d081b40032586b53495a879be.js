208ad1b9daf2ca62740eb9c9b8bd7b9c1b4ade05
chauhan463 <chauhan.11@iitj.ac.in>
2024-06-05 22:43:33 +0530

    Updates

---
+++ precache-manifest.cd77502d081b40032586b53495a879be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a39fab3809fd9ba605292a4ab8cc61",
+    "url": "/Color-picker/index.html"
+  },
+  {
+    "revision": "673ee200d228519c2c92",
+    "url": "/Color-picker/static/css/main.32edf7c4.chunk.css"
+  },
+  {
+    "revision": "8c31e15974dd7e32aed5",
+    "url": "/Color-picker/static/js/2.3c5e5abe.chunk.js"
+  },
+  {
+    "revision": "9936b74108f8b37c975a29172bd6c34e",
+    "url": "/Color-picker/static/js/2.3c5e5abe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "673ee200d228519c2c92",
+    "url": "/Color-picker/static/js/main.f93fecf2.chunk.js"
+  },
+  {
+    "revision": "f6e080937a747ca0a6ab",
+    "url": "/Color-picker/static/js/runtime-main.eb92e38f.js"
+  }
+]);
